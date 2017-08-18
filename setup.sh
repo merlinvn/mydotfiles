d0065eec2a9a61c6d03296ec6de28e276a4ae94d
merlinvn <merlinvn@gmail.com>
2017-08-18 12:51:21 +0700

    add neovim config

--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@ git clone https://github.com/k-takata/minpac.git ./vim/.vim/pack/minpac/opt/minp
 
 ln -s ~/mydotfiles/vim/.vimrc ~/.vimrc
 ln -s ~/mydotfiles/vim/.vim ~/.vim
+
+ln -s ~/mydotfiles/bash/.bash_aliases ~/.bash_aliases
+
+ln -s ~/mydotfiles/config/.config ~/.config
